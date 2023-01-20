f2376f850488ecd896a1770269f4c5f01b16ea01
HZ1Q <33613789+HZ1Q@users.noreply.github.com>
2023-01-20 18:45:43 -0500

    Update 002.bat

--- RPOS_RTS_RESTORE/002/002.bat
+++ RPOS_RTS_RESTORE/002/002.bat
@@ -1,4 +1,4 @@
-@echo on
+@echo off
 
 rem Set the directory where the ZIP files are located
 set zipfolder=%~dp0
@@ -8,10 +8,10 @@ set cnt=0
 for /f "delims=" %%i in ('dir /b /a-d "%zipfolder%\RTSBACKUP-*.zip"') do (
     set /A cnt+=1
     set "file=%%~i"
-    set "file=!file:~11!"
+    set "file=!file:~0!"
     echo !cnt! - !file!
 )
-echo.
+
 rem Prompt the user to select a file to extract
 set /p "fileNum=Enter the number of the file you want to extract: "
 
@@ -21,8 +21,8 @@ for /f "delims=" %%i in ('dir /b /a-d "%zipfolder%\RTSBACKUP-*.zip"') do (
     set /A cnt+=1
     if !cnt!==%fileNum% (
         set "file=%%~i"
-        set "file=!file:~11!"
-	start /min "***Compressing backup files...***      ***Do not close this window.***" /wait powershell Expand-Archive "%zipfolder%%FILENUM%" "%ZIPFILE%%FILENUM:~0,-1%"
+        set "file=!file:~0!"
+	start "***Compressing backup files...***      ***Do not close this window.***" /wait powershell Expand-Archive -LiteralPath "%zipfolder%%file%" -DestinationPath "%zipfolder%%File:~0,-4%
         echo The file RTSBACKUP-!file!.zip has been extracted successfully!
         goto :break
     )
